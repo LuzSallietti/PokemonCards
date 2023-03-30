4781af11b6fcb4d02e0f5ffb1b99e8b8e800f238
LuzSallietti <luzsallietti@gmail.com>
2023-03-30 12:17:27 -0300

    new unit tests added to Login

--- src/week6-tests/test/Login.test.jsx
+++ src/week6-tests/test/Login.test.jsx
@@ -1,14 +1,30 @@
 import { describe, test, expect } from 'vitest';
 import matchers from '@testing-library/jest-dom/matchers'
 import { render, screen, fireEvent } from "@testing-library/react";
+
 import Login from "../Login";
 
 expect.extend(matchers);
 
 //cuerpo del test (suites)
 
-describe("tests para el componente Login", () => {
-    test("Se renderiza el componente en el DOM", () => {
+describe("Suite de tests para el componente Login", () => {
+    test('El componente Login se renderiza sin inconvenientes', () => {
+        render(<Login />);
+      })
+    test('El input del email se renderiza correctamente', () => {
+        const { getByLabelText } = render(<Login />);
+        const emailInput = getByLabelText('Email:');
+        expect(emailInput).toBeInTheDocument();
+        expect(emailInput).toHaveAttribute('placeholder', 'Ingresa tu email');
+    })
+    test('El input del password se renderiza correctamente', () => {
+        const { getByLabelText } = render(<Login />);
+        const passwordInput = getByLabelText('Password:');
+        expect(passwordInput).toBeInTheDocument();
+        expect(passwordInput).toHaveAttribute('placeholder', 'Ingresa tu Password');
+      });
+    test("Testear el evento onchange del input name", () => {
         render(<Login/>)
         const EmailInput = screen.getByPlaceholderText("Ingresa tu email");
         expect(EmailInput).toBeDefined();
@@ -20,4 +36,15 @@ describe("tests para el componente Login", () => {
         render(<Login/>)        
         expect(screen.getByRole('button')).toHaveAttribute('type', 'submit')
     })
+    test('El formulario puede ser enviado', () => {
+        const { getByLabelText, getByText } = render(<Login />);
+        const emailInput = getByLabelText('Email:');
+        const passwordInput = getByLabelText('Password:');
+        const submitButton = getByText('Enviar');
+        
+        fireEvent.change(emailInput, { target: { value: 'test@example.com' }});
+        fireEvent.change(passwordInput, { target: { value: 'password123' }});
+        fireEvent.click(submitButton);
+      })      
 })
+
